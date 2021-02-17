efeb0643419545329a1e4262a2f74ace00a3cae2
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2021-02-18 07:03:32 +0800

    refactor code to somehow utilize vue router despite being an spa

---
+++ src/views/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    id="main-wrapper"
+    data-bs-spy="scroll"
+    data-bs-target="#main-nav"
+    data-bs-offset="0"
+    tabindex="0"
+  >
+    <Introduction />
+    <About />
+    <Projects />
+    <Contact />
+    <TheFooter />
+
+    <div class="scroll-container" v-if="visible">
+      <div class="scroll-button" v-scroll-to="'#app'">
+        <b-icon icon="caret-up-fill" variant="dark" font-scale="2"></b-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Introduction from "@/components/Introduction";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import TheFooter from "@/components/TheFooter";
+
+export default {
+  name: "index",
+  components: {
+    Introduction,
+    About,
+    Projects,
+    Contact,
+    TheFooter,
+  },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  mounted: function () {
+    window.addEventListener("scroll", this.scrollListener);
+  },
+  beforeDestroy: function () {
+    window.removeEventListener("scroll", this.scrollListener);
+  },
+  methods: {
+    scrollListener: function () {
+      this.visible = window.scrollY > 200;
+    },
+  },
+};
+</script>
