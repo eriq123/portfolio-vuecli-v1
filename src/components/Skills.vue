eba7fb5f5f052008f516c28458d83d844f9976df
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2021-02-18 06:22:43 +0800

    refactor code on skills section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,36 +3,12 @@
     <b-container>
       <h1 class="text-center section-title">Skills</h1>
       <b-row class="about-row" cols="2" cols-lg="3">
-        <b-col class="p-3 skills-column">
-          <img src="../assets/icons/php-2.png" width="150" alt="php icon" />
-        </b-col>
-        <b-col class="p-3 skills-column">
-          <img
-            src="../assets/icons/javascript-3.png"
-            width="100"
-            alt="javascript icon"
-          />
-        </b-col>
-        <b-col class="p-3 skills-column">
-          <img src="../assets/icons/css3.png" width="100" alt="css3 icon" />
-        </b-col>
-        <b-col class="p-3 skills-column">
-          <img
-            src="../assets/icons/laravel.png"
-            width="100"
-            alt="laravel icon"
-          />
-        </b-col>
-        <b-col class="skills-column">
-          <img src="../assets/icons/jquery.png" width="150" alt="jquery icon" />
-        </b-col>
-        <b-col class="p-3 skills-column">
-          <img src="../assets/icons/sass.png" width="100" alt="sass icon" />
-        </b-col>
-      </b-row>
-      <b-row class="about-row">
-        <b-col class="p-3 skills-column">
-          <img src="../assets/icons/vue.png" width="100" alt="vue icon" />
+        <b-col
+          class="p-3 skills-column"
+          v-for="(image, index) in images"
+          :key="index"
+        >
+          <img :src="image.src" :width="image.width" :alt="image.alt" />
         </b-col>
       </b-row>
     </b-container>
@@ -41,5 +17,46 @@
 <script>
 export default {
   name: "Skills",
+  data() {
+    return {
+      images: [
+        {
+          src: `${require("../assets/icons/php-2.png")}`,
+          width: "150",
+          alt: "php icon",
+        },
+        {
+          src: `${require("../assets/icons/javascript-3.png")}`,
+          width: "100",
+          alt: "javascript icon",
+        },
+        {
+          src: `${require("../assets/icons/css3.png")}`,
+          width: "100",
+          alt: "css3 icon",
+        },
+        {
+          src: `${require("../assets/icons/laravel.png")}`,
+          width: "100",
+          alt: "laravel icon",
+        },
+        {
+          src: `${require("../assets/icons/jquery.png")}`,
+          width: "150",
+          alt: "jquery icon",
+        },
+        {
+          src: `${require("../assets/icons/sass.png")}`,
+          width: "100",
+          alt: "sass icon",
+        },
+        {
+          src: `${require("../assets/icons/vue.png")}`,
+          width: "100",
+          alt: "vue icon",
+        },
+      ],
+    };
+  },
 };
 </script>
