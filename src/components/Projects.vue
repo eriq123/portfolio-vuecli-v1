af63291202fd3dfe1f09b2b28ec7d0926dd37599
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2021-02-21 12:49:16 +0800

    added hover function on projects section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,15 +11,31 @@
           <b-col
             v-for="project in projects"
             :key="project.name"
-            class="p-3 project-column"
+            class="project-column"
           >
             <b-card
-              :title="project.name"
               :img-src="project.thumbnail"
               :img-alt="project.name"
               img-top
+              class="project-card"
+              v-if="project.show"
+              @mouseover="project.show = !project.show"
             >
             </b-card>
+
+            <div
+              class="project-hovered"
+              v-if="!project.show"
+              @mouseleave="project.show = !project.show"
+            >
+              <div class="project-text">
+                <p class="project-name">{{ project.name }}</p>
+                <p class="tech-name text-secondary">{{ project.stack }}</p>
+              </div>
+              <div class="project-button">
+                <b-button variant="outline-primary">Learn More</b-button>
+              </div>
+            </div>
           </b-col>
         </b-row>
       </b-container>
@@ -32,24 +48,34 @@ export default {
     return {
       projects: [
         {
+          show: true,
           name: "Beauty N' Bottles version 1",
           thumbnail: `${require("@/assets/projects/beautynbottles-v1/home.png")}`,
+          stack: "Laravel / Bootstrap 4",
         },
         {
+          show: true,
           name: "Beauty N' Bottles version 2",
           thumbnail: `${require("@/assets/projects/beautynbottles-v2/home.png")}`,
+          stack: "Laravel / Vue JS / Vuetify",
         },
         {
+          show: true,
           name: "Calendar Event App",
           thumbnail: `${require("@/assets/projects/appetizer-event-exam/home.png")}`,
+          stack: "Laravel / Vue JS",
         },
         {
+          show: true,
           name: "Animal Shelter",
           thumbnail: `${require("@/assets/projects/animal-shelter/home.png")}`,
+          stack: "Laravel / Bootstrap 5 / SASS",
         },
         {
+          show: true,
           name: "Expense Manager",
           thumbnail: `${require("@/assets/projects/expense-manager/home.png")}`,
+          stack: "Laravel / jQuery",
         },
       ],
     };
