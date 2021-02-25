d0fc39bc3e8cd0844e890deaafc247f3b5e896ce
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2021-02-25 09:44:11 +0800

    added description to projects and rename sub category image

--- src/components/ProjectCardContent.vue
+++ src/components/ProjectCardContent.vue
@@ -1,18 +1,15 @@
 <template>
-  <b-card-body :title="title" class="d-flex flex-column h-100">
+  <b-card-body :title="title">
     <p class="card-subtitle text-primary">
       {{ stack }}
     </p>
     <p class="card-description">
-      This is just a short description made by the brilliant developer Eriq
-      John. This is just a filler of this huge space to make it look aesthetic
-      for now.
+      <slot>
+        This is just a short description made by the brilliant developer Eriq
+        John. This is just a filler of this huge space to make it look aesthetic
+        for now.
+      </slot>
     </p>
-    <div class="mt-auto text-center">
-      <b-button variant="outline-primary" @click="$emit('open')">
-        Learn More
-      </b-button>
-    </div>
   </b-card-body>
 </template>
 <script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,28 +13,50 @@
             :key="project.name"
             class="project-column"
           >
-            <b-card no-body class="overflow-hidden">
+            <b-card no-body class="overflow-hidden" @click="openModal(project)">
               <b-row no-gutters>
                 <b-col md="6" class="d-none d-md-block">
                   <project-card-content
                     :title="project.name | capitalize"
                     :stack="project.stack"
-                    @open="openModal(project)"
-                  />
+                  >
+                    <template v-if="project.description">
+                      {{ project.description }}
+                    </template>
+                  </project-card-content>
                 </b-col>
                 <b-col md="6">
-                  <b-card-img
-                    :src="project.thumbnail"
-                    :alt="project.name"
-                    class="rounded-0 project-image"
-                  ></b-card-img>
+                  <b-carousel
+                    :interval="2000"
+                    controls
+                    indicators
+                    background="#ababab"
+                    fade
+                  >
+                    <b-carousel-slide
+                      v-for="(image, index) in project.images"
+                      :key="index"
+                    >
+                      <template #img>
+                        <img
+                          class="d-block w-100 project-image"
+                          width="1024"
+                          :src="image.src"
+                          :alt="`${modal.title} images`"
+                        />
+                      </template>
+                    </b-carousel-slide>
+                  </b-carousel>
                 </b-col>
                 <b-col md="6" class="d-md-none">
                   <project-card-content
                     :title="project.name | capitalize"
                     :stack="project.stack"
-                    @open="openModal(project)"
-                  />
+                  >
+                    <template v-if="project.description">
+                      {{ project.description }}
+                    </template>
+                  </project-card-content>
                 </b-col>
               </b-row>
             </b-card>
@@ -42,8 +64,34 @@
         </b-row>
       </b-container>
 
-      <b-modal id="project-modal" hide-footer centered :title="modal.title">
-        <p class="my-4">A carousel of {{ modal.title }} will be here soon.</p>
+      <b-modal
+        id="project-modal"
+        hide-footer
+        centered
+        size="xl"
+        body-class="p-0"
+        :title="modal.title | capitalize"
+      >
+        <b-carousel
+          id="carousel-1"
+          :interval="2000"
+          controls
+          indicators
+          background="#ababab"
+          fade
+          no-hover-pause
+        >
+          <b-carousel-slide v-for="(image, index) in modal.images" :key="index">
+            <template #img>
+              <img
+                class="d-block w-100 h-480"
+                width="1024"
+                :src="image.src"
+                :alt="`${modal.title} images`"
+              />
+            </template>
+          </b-carousel-slide>
+        </b-carousel>
       </b-modal>
     </div>
   </section>
@@ -59,34 +107,134 @@ export default {
           name: "BnB Inventory version 1",
           thumbnail: `${require("@/assets/projects/beautynbottles-v1/home.png")}`,
           stack: "Laravel / Bootstrap 4",
+          description:
+            "This is my first freelance project, an inventory system which was requested by a perfume company Beauty n Bottles. I used bootstrap 3 as my main css framework and laravel as my backend.",
+          images: [
+            {
+              src: `${require("@/assets/projects/beautynbottles-v1/home.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v1/accounts.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v1/category.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v1/convert-stocks.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v1/report.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v1/login.png")}`,
+            },
+          ],
         },
         {
           show: true,
           name: "BnB Inventory version 2",
           thumbnail: `${require("@/assets/projects/beautynbottles-v2/home.png")}`,
           stack: "Laravel / Vue JS / Vuetify",
+          description:
+            "This is my latest project which is the same inventory system but with a lot of different functionalities. The main purpose of the second version is to provide a report for easy quarter audit.",
+          images: [
+            {
+              src: `${require("@/assets/projects/beautynbottles-v2/home.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v2/raw.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v2/raw-update.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v2/assembly.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v2/flow.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/beautynbottles-v2/report.png")}`,
+            },
+          ],
         },
         {
           show: true,
           name: "Calendar Event",
           thumbnail: `${require("@/assets/projects/appetizer-event-exam/home.png")}`,
           stack: "Laravel / Vue JS",
+          description:
+            "Calendar event is an application made to easily track events that provides a nice user interface with the help of vue components. This is my first Vue JS application that i submitted to a company as part of the coding examination.",
+          images: [
+            {
+              src: `${require("@/assets/projects/appetizer-event-exam/home.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/appetizer-event-exam/home-success.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/appetizer-event-exam/home-with-data.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/appetizer-event-exam/home-with-data-2.png")}`,
+            },
+          ],
         },
         {
           show: true,
           name: "Animal Shelter",
           thumbnail: `${require("@/assets/projects/animal-shelter/home.png")}`,
           stack: "Laravel / Bootstrap 5 / SASS",
+          description:
+            "Animal Shelter is one of my freelance projects which helps a business to monitor rescued animals. It also monitors rescuers, adopters, donations etc. This is quite a small project but it can also help developers who are just starting with laravel since it has a lot of CRUD.",
+          images: [
+            {
+              src: `${require("@/assets/projects/animal-shelter/home.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/animal-shelter/animals.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/animal-shelter/animals-read.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/animal-shelter/animals-add.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/animal-shelter/rescuers.png")}`,
+            },
+          ],
         },
         {
           show: true,
           name: "Expense Manager",
           thumbnail: `${require("@/assets/projects/expense-manager/home.png")}`,
-          stack: "Laravel / jQuery",
+          stack: "Laravel / Bootstrap 3 / jQuery",
+          description:
+            "Expense Manager got me my first job. This is part of a coding examination which consist of atleast 4 CRUD and role based application.",
+          images: [
+            {
+              src: `${require("@/assets/projects/expense-manager/home.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/expense-manager/category.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/expense-manager/expenses.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/expense-manager/roles.png")}`,
+            },
+            {
+              src: `${require("@/assets/projects/expense-manager/users.png")}`,
+            },
+          ],
         },
       ],
       modal: {
         title: null,
+        stack: null,
+        images: [],
       },
     };
   },
@@ -95,14 +243,16 @@ export default {
   },
   methods: {
     openModal(project) {
-      this.modal.title = null;
+      this.modal.description = this.modal.title = this.modal.stack = null;
       this.modal.title = project.name;
+      this.modal.stack = project.stack;
+      this.modal.images = project.images;
       this.$root.$emit("bv::show::modal", "project-modal");
     },
   },
   filters: {
     capitalize(value) {
-      return value.toUpperCase();
+      return value ? value.toUpperCase() : "";
     },
   },
 };
